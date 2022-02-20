73efcbb064c035eeca52bcf6730660407a8c4c1e
tommaay <tommy@tomster.dev>
2022-02-20 15:45:55 -0800

    Use isLoading status from the query info

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import axios from "axios"
 import { useQuery } from "react-query"
 
 export default function Home() {
-  const queryInfo = useQuery("pokemons", () =>
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results)
-  )
+  const queryInfo = useQuery("pokemons", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results)
+  })
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
-      <h1>React Query</h1>
-      {queryInfo?.data.map((pokemon) => (
-        <p key={pokemon.name}>{pokemon.name}</p>
-      ))}
+    <div className="max-w-6xl mx-auto p-4 text-center">
+      <h1 className="text-xl font-bold mb-4">React Query</h1>
+      {queryInfo.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        queryInfo.data?.map((pokemon) => <p key={pokemon.name}>{pokemon.name}</p>)
+      )}
     </div>
   )
 }
