04a76f635d3bb6154811ab54bd35ee9448b43c46
tommaay <tommy@tomster.dev>
2022-02-20 14:23:00 -0800

    Fetching data with useQuery and displaying the pokemon names

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { useState } from "react"
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query"
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
 export default function Home() {
+  const queryInfo = useQuery("pokemons", () =>
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results)
+  )
+
   return (
     <div className="max-w-6xl mx-auto p-4">
       <h1>React Query</h1>
+      {queryInfo?.data.map((pokemon) => (
+        <p key={pokemon.name}>{pokemon.name}</p>
+      ))}
     </div>
   )
 }
